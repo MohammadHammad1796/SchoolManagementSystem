679ed4bb85bd6a44ffd70f2bd28df3da916bf54d
Mohammad Hammad <mohammadhammad1796@gmail.com>
2023-09-16 01:19:03 +0300

    Prepare gateway and simple test for it.

---
+++ Admission/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Admission.Controllers;
+
+[Route("test")]
+public class TestController : Controller
+{
+	[HttpGet("test")]
+	public IActionResult Test()
+	{
+		return Ok($"{AppDomain.CurrentDomain.FriendlyName} test succeeded.");
+	}
+}
--- Attendance/Program.cs
+++ Attendance/Program.cs
@@ -1,15 +1,46 @@
-namespace Attendance
+namespace Attendance;
+
+public class Program
 {
-	public class Program
+	public static void Main(string[] args)
 	{
-		public static void Main(string[] args)
+		var builder = WebApplication.CreateBuilder(args);
+
+		ConfigureServices(builder.Services, builder.Configuration);
+
+		var app = builder.Build();
+		ConfigureApplication(app);
+
+		app.Run();
+	}
+
+	private static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+	{
+		services.AddCors(options =>
 		{
-			var builder = WebApplication.CreateBuilder(args);
-			var app = builder.Build();
+			options.AddPolicy(name: "AllowedOrigins",
+				builder =>
+				{
+					builder.WithOrigins(configuration.GetSection("AllowedOrigins").Get<string[]>()).AllowAnyHeader().AllowAnyMethod();
+				});
+		});
 
-			app.MapGet("/", () => "Hello World!");
+		services.AddControllers();
+	}
 
-			app.Run();
-		}
+	private static void ConfigureApplication(WebApplication application)
+	{
+		if (application.Environment.IsDevelopment())
+			application.UseDeveloperExceptionPage();
+
+		application.UseCors("AllowedOrigins");
+
+		application.UseRouting();
+		application.UseEndpoints(endpoints =>
+		{
+			endpoints.MapControllerRoute(
+				name: "default",
+				pattern: "{controller}/{action}/{id?}");
+		});
 	}
 }
